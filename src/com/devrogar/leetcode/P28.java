aca9d6b3dd60c4108b93fdccf5c1601c007ffb3c
Rohit G <bablu.rohit@hotmail.com>
2020-05-10 01:54:37 +0530

    P28 Solution

--- src/com/devrogar/leetcode/P28.java
+++ src/com/devrogar/leetcode/P28.java
@@ -20,28 +20,27 @@ public class P28 {
 		start = System.currentTimeMillis();
 		System.out.println(solution.strStr("mississippi", "issip") + " -> " + (System.currentTimeMillis()-start) + "ms");
 	}
+	
+	private static class Solution {
 
-
-}
-
-class Solution {
-
-	public int strStr(String haystack, String needle) {
-		if (needle.length() == 0) return 0;
-		int idx = -1;
-		for (int i = 0;i<=haystack.length()-needle.length();i++) {
-			int j = 0;
-			for (;j<needle.length();j++) {
-				if (haystack.charAt(i+j) != needle.charAt(j)) {
+		public int strStr(String haystack, String needle) {
+			if (needle.length() == 0) return 0;
+			int idx = -1;
+			for (int i = 0;i<=haystack.length()-needle.length();i++) {
+				int j = 0;
+				for (;j<needle.length();j++) {
+					if (haystack.charAt(i+j) != needle.charAt(j)) {
+						break;
+					}
+				}
+				if (j == needle.length()) {
+					idx = i;
 					break;
 				}
 			}
-			if (j == needle.length()) {
-				idx = i;
-				break;
-			}
+			return idx;
 		}
-		return idx;
+		
 	}
 	
 }
